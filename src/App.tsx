6043f8b4cb78bf643c9aef521d0a132a0b7628ab
Kayo Silva <91280312+KayoSilva19@users.noreply.github.com>
2022-06-22 13:38:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react";
+import { Event } from "./pages/Event";
+
+function App() {
+  return (
+    <Event />
+  )
+}
+
+export default App
